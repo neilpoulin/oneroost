12246b50a03389a0707489e81361564410824537
Neil Poulin <neil@neilpoulin.com>
2015-08-26 22:16:04 -0600

    cloud code and everythign!

---
+++ cloud/app.js
@@ -0,0 +1,25 @@
+var express = require('express');
+var app = express();
+
+// Global app configuration section.
+app.use(express.bodyParser());
+
+app.set('views', 'cloud/views');
+app.set('view engine', 'ejs');
+
+// Register request handlers for each route.
+app.get('/hello/:msg', function(req, res) {
+
+    res.render("hello", {name: req.query.name, msg: req.params.msg}, function(err, html) {
+        // err is set if template rendering failed.
+        // html is the output string produced by rendering the template.
+
+        res.send(html); // You need to send the response manually.
+    });
+});
+
+app.get('/test', function(request, response){
+   response.redirect('/html/index.html', 301);
+});
+
+app.listen();
---
+++ public/js/initialize.js
@@ -0,0 +1,7 @@
+$(function(){
+    Parse.$ = jQuery;
+
+    // Initialize Parse with your Parse application javascript keys
+    Parse.initialize("lSNtmvBTimEY6VfOo5zvvOQkljcHeDIOQcjefNUu",
+        "EZKlfRO9ydZrpO2fpLkIRNTp9dEJxF4IyTh4VkWT");
+});
