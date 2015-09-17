5274e3ae3b4a5a02ebb6e745d1c1cbf32e59607b
Neil Poulin <neil.j.poulin@gmail.com>
2015-09-17 00:15:45 -0600

    cleaning up unused files

--- cloud/app.js
+++ cloud/app.js
@@ -17,17 +17,6 @@ app.locals.formatTime = function(time) {
   return moment(time).format('MMMM Do YYYY, h:mm a');
 };
 
-// Register request handlers for each route.
-app.get('/hello/:msg', function(req, res) {
-
-    res.render("hello", {name: req.query.name, msg: req.params.msg}, function(err, html) {
-        // err is set if template rendering failed.
-        // html is the output string produced by rendering the template.
-
-        res.send(html); // You need to send the response manually.
-    });
-});
-
 app.get('/todo', function(request, response){
   //  response.redirect('/html/index.html', 301);
     response.render( 'todo.ejs', {} );
