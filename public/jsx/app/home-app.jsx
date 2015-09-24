40de3d56af5397c22378c5c63b135aad5818eadc
Neil Poulin <neil.j.poulin@gmail.com>
2015-09-24 01:21:44 -0600

    check for prod version an use correct keys

--- public/jsx/app/home-app.jsx
+++ public/jsx/app/home-app.jsx
@@ -1,6 +1,14 @@
 define( ['parse', 'react', 'pages/HomePage'], function( Parse, React, HomePage){
     Parse.$ = jQuery;
-    Parse.initialize("lSNtmvBTimEY6VfOo5zvvOQkljcHeDIOQcjefNUu", "EZKlfRO9ydZrpO2fpLkIRNTp9dEJxF4IyTh4VkWT");
+    if ( window.location.hostname.indexOf("dev") == -1 ) //prod
+    {
+      Parse.initialize("lSNtmvBTimEY6VfOo5zvvOQkljcHeDIOQcjefNUu", "EZKlfRO9ydZrpO2fpLkIRNTp9dEJxF4IyTh4VkWT");
+    }
+    else
+    {
+      Parse.initialize("TFy4TyyJJGpG7gnOUWzOZNtMcCkqQlYTfa4mJWQq", "CZfXoAnHhHU46Id1GBZ0zB9LFKHZI0HZJt1GfTlo");
+    }
+
 
     React.render(<HomePage />, document.getElementById('homePage'));
 });
--- public/jsx/app/user/UserHomeApp.jsx
+++ public/jsx/app/user/UserHomeApp.jsx
@@ -1,6 +1,13 @@
 define( ['parse', 'react', 'pages/UserHomePage'], function( Parse, React, UserHomePage){
-    Parse.$ = jQuery;
-    Parse.initialize("lSNtmvBTimEY6VfOo5zvvOQkljcHeDIOQcjefNUu", "EZKlfRO9ydZrpO2fpLkIRNTp9dEJxF4IyTh4VkWT");
+      Parse.$ = jQuery;
+      if ( window.location.hostname.indexOf("dev") == -1 ) //prod
+      {
+        Parse.initialize("lSNtmvBTimEY6VfOo5zvvOQkljcHeDIOQcjefNUu", "EZKlfRO9ydZrpO2fpLkIRNTp9dEJxF4IyTh4VkWT");
+      }
+      else
+      {
+        Parse.initialize("TFy4TyyJJGpG7gnOUWzOZNtMcCkqQlYTfa4mJWQq", "CZfXoAnHhHU46Id1GBZ0zB9LFKHZI0HZJt1GfTlo");
+      }
 
       var Account = Parse.Object.extend("Account");
 
