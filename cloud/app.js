b1d6dc55c89719f5d7ecba843ca74000b095a318
Neil Poulin <neil.j.poulin@gmail.com>
2015-10-13 20:33:18 -0600

    add javascript to construction page

--- cloud/app.js
+++ cloud/app.js
@@ -19,13 +19,37 @@ app.locals.formatTime = function(time) {
   return moment(time).format('MMMM Do YYYY, h:mm a');
 };
 
+var parseConfig = Parse.Config.current();
+
 // app.get("/", usersController.index);
 app.get("/", function( request, response ){
     var env = envUtil.getEnv();
     var homePage = env.isDev ? "home.ejs" : "construction.ejs";
+    var params = env.json;
+    getConfig();
+    console.log(parseConfig);
+    console.log( parseConfig.get( "landing_motto" ) ) ;
 
-    response.render( homePage, env.json );
+    response.render( homePage, params);
 });
+
+
+function getConfig()
+{
+  console.log("attempting to get config");
+  Parse.Config.get().then( function(config){
+    console.log("Successfully retrieved new config");
+    console.log(config);
+    parseConfig = config;
+  },
+  function(error){
+    console.log("error... failed to get config");
+    console.log(error);
+    parseConfig = Parse.Config.current();
+  });
+
+  return parseConfig;
+}
 
 app.get("/my/home", userController.getMyHome );
 
---
+++ dev-start.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+clear
+
+echo "starting babel..."
+babel watch jsx/ --out-dir out/js/ &
