a17fcbef868bfc965452dbb5f507ddbd1ff8df82
Neil Poulin <neil@sympoz.com>
2015-11-18 23:10:07 -0700

    styling update

--- public/jsx/components/sidebar/Menu.jsx
+++ public/jsx/components/sidebar/Menu.jsx
@@ -1,8 +1,10 @@
-define( ['react', 'sidebar/MenuItem'], function( React, MenuItem ){
+define( ['react', 'sidebar/MenuItem', 'jquery'], function( React, MenuItem, $ ){
     return React.createClass({
+        mixins: [React.addons.LinkedStateMixin],
         getInitialState: function() {
             return {
-                visible: false
+                visible: false,
+                filterTerm: ""
             };
         },
         toggle: function()
@@ -11,17 +13,37 @@ define( ['react', 'sidebar/MenuItem'], function( React, MenuItem ){
         },
         show: function() {
             this.setState({ visible: true });
-            document.addEventListener("click", this.hide );
+            document.addEventListener( "click", this.handleDocumentClick );
         },
         hide: function() {
-            document.removeEventListener("click", this.hide );
+            document.removeEventListener( "click", this.handleDocumentClick );
             this.setState({ visible: false });
         },
-
+        handleDocumentClick: function( e ){
+            if ( !$(e.target).closest( ".menu" ).length )
+            {
+                this.hide();
+            }
+        },
+        doFilter: function(){
+            console.log("filtering: " + this.state.filterTerm );
+        },
         render: function() {
-            return <div className="menu">
-                <div className={(this.state.visible ? "visible " : "") + this.props.alignment}>{this.props.children}</div>
-            </div>;
+            return (
+                <div className="menu">
+                    <div className={(this.state.visible ? "visible " : "") + this.props.alignment}>
+                        <div className="menu-search">
+                            <div className="form-group">
+                                <div className="inner-addon left-addon">
+                                    <div className="inner-addon left-addon"><i className="fa fa-search"></i></div>
+                                    <input type="text" className="form-control" placeholder="search" onKeyUp={this.doFilter} valueLink={this.linkState('filterTerm')} />
+                                </div>
+                            </div>
+                        </div>
+                        {this.props.children}
+                    </div>
+                </div>
+            );
         }
     });
 });
