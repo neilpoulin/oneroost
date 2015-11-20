4573cdc5477a9865956648eb449eeb73ea48e132
Neil Poulin <neil@sympoz.com>
2015-11-19 21:48:39 -0700

    fix positioning of comments section

--- public/jsx/components/deal/Comments.jsx
+++ public/jsx/components/deal/Comments.jsx
@@ -1,54 +1,73 @@
 define( ['underscore', 'react', 'parse', 'parse-react', 'models/Deal', 'models/DealComment', 'deal/AddComment'],
 function(_, React, Parse, ParseReact, Deal, DealComment, AddComment){
-  return React.createClass({
-      mixins: [ParseReact.Mixin],
-      observe: function(){
-        return {
-          dealComments: new Parse.Query('DealComment').equalTo( 'deal', this.props.deal ).ascending('createdAt')
-        }
-      },
-      addComment: function( comment )
-      {
-          this.refreshQueries('dealComments');
-          this.render();
-      },
-      formatCommentDate: function( comment )
-      {
-        var date = comment.createdAt;
-        return date.toLocaleString();
-      },
-      componentDidUpdate: function()
-      {
-          var d = $(this.refs.commentList.getDOMNode());
-          d.scrollTop(d.prop("scrollHeight"));
-      },
-      render: function(){
-          var component = this;
-          var deal = this.props.deal;
-          var comments = this.data.dealComments;
-          return (
-              <div>
-                  <h2>Comments</h2>
-                  <div className="commentsSection row-fluid">
-                    <div className="container-fluid">
-                        <AddComment
-                            ref="addComment"
-                            deal={deal}
-                            addComment={this.addComment} >
-                        </AddComment>
+    return React.createClass({
+        mixins: [ParseReact.Mixin],
+        observe: function(){
+            return {
+                dealComments: new Parse.Query('DealComment').equalTo( 'deal', this.props.deal ).ascending('createdAt')
+            }
+        },
+        addComment: function( comment )
+        {
+            this.refreshQueries('dealComments');
+            this.render();
+        },
+        formatCommentDate: function( comment )
+        {
+            var date = comment.createdAt;
+            return date.toLocaleString();
+        },
+        componentDidMount: function() {
+            window.addEventListener("resize", this.updateDimensions);
+        },
+        componentWillUnmount: function() {
+            window.removeEventListener("resize", this.updateDimensions);
+        },
+        componentDidUpdate: function()
+        {
+            this.updateDimensions();
+        },
+        updateDimensions: function(){
+            var $commentList = $(this.refs.commentList.getDOMNode());
+            $commentList.scrollTop( $commentList.prop("scrollHeight") );
 
-                        <ul className="list-unstyled" id="commentsContainer" ref="commentList">
-                          {comments.map(function(comment){
-                              return <li className="comment hover-effects">
-                                  <span className="username">{comment.username}</span>:&nbsp;<span className="message">{comment.message}</span>
-                                  <span className="postTime hover-show">{component.formatCommentDate(comment)}</span>
-                              </li>
-                          })}
-                        </ul>
+            var commentsTopPx = $(".commentsSection").position().top;
+            var commentsHeaderHeightPx = $(".commentsHeader").outerHeight();
+
+            var $msgContainer = $(".messagesContainer");
+            var messageListHeight = commentsTopPx + commentsHeaderHeightPx;
+            $msgContainer.css({top: messageListHeight });
+        },
+        render: function(){
+            var component = this;
+            var deal = this.props.deal;
+            var comments = this.data.dealComments;
+            return (
+                <div className="commentsSection">
+                    <div className="commentsHeader">
+                        <h2>Comments</h2>
                     </div>
-                  </div>
-              </div>
+                    <div className="row-fluid">
+                        <div className="messagesContainer">
+                            <div className="container">
+                                <ul className="list-unstyled" id="commentsList" ref="commentList">
+                                    {comments.map(function(comment){
+                                        return <li className="comment hover-effects">
+                                            <span className="username">{comment.username}</span>:&nbsp;<span className="message">{comment.message}</span>
+                                            <span className="postTime hover-show">{component.formatCommentDate(comment)}</span>
+                                        </li>
+                                    })}
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <AddComment
+                        ref="addComment"
+                        deal={deal}
+                        addComment={this.addComment} >
+                    </AddComment>
+                </div>
             );
-      }
-  });
+        }
+    });
 })
