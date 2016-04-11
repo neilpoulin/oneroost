19e9444fe07980a985dc64f1c9c51927fa94f8a0
Neil Poulin <neil@sympoz.com>
2016-04-10 23:09:19 -0600

    fix stakeholder sidebar thing not rendering, fix bootstrap jquery

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 'use strict'
 var gulp = require('gulp');
 var babel = require("gulp-babel");
-var browserify = require("gulp-browserify");
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var del = require("del");
@@ -14,143 +13,143 @@ var buffer = require('vinyl-buffer');
 var browserify = require('browserify');
 var less = require('gulp-less');
 var merge = require('merge-stream');
-
-
+var jQuery = require('jquery');
 
 var bootstrapRoot = './node_modules/bootstrap-sass/';
 var bootstrapPaths = {
-    fonts: bootstrapRoot + 'assets/fonts/**/*',
-    stylesheets: bootstrapRoot + 'assets/stylesheets'
+  fonts: bootstrapRoot + 'assets/fonts/**/*',
+  stylesheets: bootstrapRoot + 'assets/stylesheets'
 }
 
 var fontAwesomeRoot = './node_modules/font-awesome/';
 var fontAwesomePaths = {
-    fonts: fontAwesomeRoot + 'fonts/**/*',
-    stylesheets: fontAwesomeRoot + 'scss'
+  fonts: fontAwesomeRoot + 'fonts/**/*',
+  stylesheets: fontAwesomeRoot + 'scss'
 }
 
 var materialColorsRoot = './node_modules/sass-material-colors/';
 var GoogleMaterialColors = {
-    stylesheets: materialColorsRoot + 'sass'
+  stylesheets: materialColorsRoot + 'sass'
 }
 
 var reactModalBootstrap = {
-    stylesheets: './node_modules/react-bootstrap-modal/lib/styles/rbm-patch.less'
+  stylesheets: './node_modules/react-bootstrap-modal/lib/styles/rbm-patch.less'
 }
 
 var paths = {
-    src: {
-        root: './src',
-        scripts: ['./src/**/*.jsx',
-            './src/**/*.js',
-            './node_modules/jquery/jquery.min.js',
-            './node_modules/bootstrap/dist/js/bootstrap.min.js'],
-        styles: ['./src/scss/**/*.scss'],
-        styleEntry: './src/scss/index.scss',
-        fonts: [bootstrapPaths.fonts, fontAwesomePaths.fonts]
-    },
-    build: {
-        root: './build',
-        js: './build/js',
-        sourceFile: './build/js/jsx/index.js',
-        sourceMaps: '.build/**/*.js.map'
-    },
-    dest: {
-        root: './public',
-        css: './public/css',
-        js: './public/bundle',
-        fonts: './public/css/fonts',
-        styleName: 'styles.css',
-        scriptName: 'bundle.js'
-    }
+  src: {
+    root: './src',
+    scripts: ['./src/**/*.jsx',
+      './src/**/*.js'],
+    styles: ['./src/scss/**/*.scss'],
+    styleEntry: './src/scss/index.scss',
+    fonts: [bootstrapPaths.fonts, fontAwesomePaths.fonts]
+  },
+  build: {
+    root: './build',
+    js: './build/js',
+    sourceFile: './build/js/jsx/index.js',
+    sourceMaps: '.build/**/*.js.map'
+  },
+  dest: {
+    root: './public',
+    css: './public/css',
+    js: './public/bundle',
+    fonts: './public/css/fonts',
+    styleName: 'styles.css',
+    scriptName: 'bundle.js'
+  }
 };
 
 var sassOpts = {
-    outputStyle: 'nested',
-    precison: 3,
-    errLogToConsole: true,
-    includePaths: [bootstrapPaths.stylesheets,
-        fontAwesomePaths.stylesheets,
-        GoogleMaterialColors.stylesheets,
-        reactModalBootstrap.stylesheets,
-        './src/scss/**/*.scss']
+  outputStyle: 'nested',
+  precison: 3,
+  errLogToConsole: true,
+  includePaths: [bootstrapPaths.stylesheets,
+    fontAwesomePaths.stylesheets,
+    GoogleMaterialColors.stylesheets,
+    reactModalBootstrap.stylesheets,
+    './src/scss/**/*.scss']
 };
 
-gulp.task('fonts', function(){
-    return gulp.src( paths.src.fonts )
-    .pipe( gulp.dest( paths.dest.fonts ) );
+gulp.task('fonts', function () {
+  return gulp.src(paths.src.fonts)
+    .pipe(gulp.dest(paths.dest.fonts));
 });
 
-gulp.task('sass', ['clean', 'fonts'], function(){
-    var scssStream = gulp.src( paths.src.styleEntry )
-        .pipe(sourcemaps.init())
-        .pipe( sass( sassOpts ).on( 'error', sass.logError ) )
-        .pipe( sourcemaps.write('./maps') )
-        .pipe( concat( paths.dest.styleName ) );
+gulp.task('sass', ['clean', 'fonts'], function () {
+  var scssStream = gulp.src(paths.src.styleEntry)
+    .pipe(sourcemaps.init())
+    .pipe(sass(sassOpts).on('error', sass.logError))
+    .pipe(sourcemaps.write('./maps'))
+    .pipe(concat(paths.dest.styleName));
 
-    var lessStream = gulp.src(reactModalBootstrap.stylesheets)
-        .pipe(less())
-        .pipe(concat('less-files.less'));
+  var lessStream = gulp.src(reactModalBootstrap.stylesheets)
+    .pipe(less())
+    .pipe(concat('less-files.less'));
 
-    var mergedStream = merge( scssStream, lessStream )
-        .pipe(concat('styles.css'))
-        .pipe( gulp.dest( paths.dest.css ) );
+  var mergedStream = merge(scssStream, lessStream)
+    .pipe(concat('styles.css'))
+    .pipe(gulp.dest(paths.dest.css));
 
-    return mergedStream;
+  return mergedStream;
 });
 
-gulp.task('clean', function(){
-    return del( [paths.build.root] );
+gulp.task('clean', function () {
+  return del([paths.build.root]);
 });
 
-gulp.task('clean:all', ['clean'], function(){
-    return del(['./public/bundle', './public/css']);
+gulp.task('clean:all', ['clean'], function () {
+  return del(['./public/bundle', './public/css']);
 });
 
-gulp.task('transpile', ['clean'], function(){
-    return gulp.src( paths.src.scripts )
-    .pipe( plumber() )
-    .pipe( sourcemaps.init({ loadMaps: true }) )
-    .pipe( babel() )
-    .on('error', function(err){
-        gutil.log( gutil.colors.red('[Task "transpile"][Babel Error]') );
-        gutil.log( gutil.colors.red(err.message) );
+gulp.task('transpile', ['clean'], function () {
+  return gulp.src(paths.src.scripts)
+    .pipe(plumber())
+    .pipe(sourcemaps.init({loadMaps: true}))
+    .pipe(babel())
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Task "transpile"][Babel Error]'));
+      gutil.log(gutil.colors.red(err.message));
     })
-    .pipe( sourcemaps.write() )
-    .pipe( plumber.stop() )
-    .pipe( gulp.dest( paths.build.js ));
+    .pipe(sourcemaps.write())
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(paths.build.js));
 });
 
-gulp.task('bundle', ['clean', 'transpile' ], function(){
-    var b = browserify({
-      entries: paths.build.sourceFile,
-      debug: true
-    });
+gulp.task('bundle', ['clean', 'transpile'], function () {
+  var b = browserify({
+    entries: paths.build.sourceFile,
+    debug: true
+  });
 
-    return plumber()
-    .pipe(b.bundle() )
-    .pipe( source(paths.build.sourceFile) )
-    .pipe( buffer() )
-    .pipe( sourcemaps.init({ loadMaps: true }) )
+  return plumber()
+    .pipe(b.bundle()
+      .on('error', function (err) {
+        gutil.log(gutil.colors.red('[Task "bundle"][Browserify.bundle() Error]'));
+        gutil.log(gutil.colors.red(err.message));
+      }))
+    .pipe(source(paths.build.sourceFile))
+    .pipe(buffer())
+    .pipe(sourcemaps.init({loadMaps: true}))
     // .pipe( browserify({debug: true}) )
-    .on('error', function(err){
-        gutil.log( gutil.colors.red('[Task "bundle"][Browserify Error]') );
-        gutil.log( gutil.colors.red(err.message) );
+    .on('error', function (err) {
+      gutil.log(gutil.colors.red('[Task "bundle"][Browserify Error]'));
+      gutil.log(gutil.colors.red(err.message));
     })
-    .pipe( plumber.stop() )
-    .pipe( concat( paths.dest.scriptName ) )
+    .pipe(concat(paths.dest.scriptName))
     // .pipe( sourcemaps.write('./maps') )
-    .pipe( plumber.stop() )
-    .pipe( gulp.dest(paths.dest.js) );
+    .pipe(plumber.stop())
+    .pipe(gulp.dest(paths.dest.js));
 });
 
 gulp.task('build', ['clean', 'bundle', 'sass']);
 
-gulp.task('parse-develop', shell.task('parse develop dev') );
+gulp.task('parse-develop', shell.task('parse develop dev'));
 
-gulp.task('watch', ['clean', 'build'], function(){
-    gulp.watch( paths.src.styles, ['sass'] );
-    gulp.watch( paths.src.scripts, ['bundle'] );
-    gulp.watch( paths.src.fonts, ['fonts'] );
+gulp.task('watch', ['clean', 'build'], function () {
+  gulp.watch(paths.src.styles, ['sass']);
+  gulp.watch(paths.src.scripts, ['bundle']);
+  gulp.watch(paths.src.fonts, ['fonts']);
 });
 gulp.task('develop', ['watch', 'parse-develop']);
--- src/jsx/modules/navigation/TopNav.jsx
+++ src/jsx/modules/navigation/TopNav.jsx
@@ -2,40 +2,41 @@ import React, { PropTypes } from 'react'
 import Parse from 'parse';
 import AnonymousLinks from './AnonymousLinks';
 import LoggedInLinks from './LoggedInLinks';
-import jQuery from 'jquery'
+window.$ = window.jQuery = require('jquery');
+window.Tether = require('tether');
 import Bootstrap from 'bootstrap'
 
 
 const TopNav = React.createClass({
-    isLoggedIn: function(){
-        return Parse.User.current() != null;
-    },
-    componentDidMount(){
-        jQuery('.collapse').collapse();
-    },
-    render: function() {
-        var links = (<AnonymousLinks/>);
-        if ( this.isLoggedIn() )
-        {
-            links = (<LoggedInLinks user={Parse.User.current()} />);
-        }
-
-        return (
-            <nav className="navbar navbar-default navbar-fixed-top">
-                <div className="container-fluid">
-                    <div className="navbar-header">
-                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-                            <span className="sr-only">Toggle navigation</span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                            <span className="icon-bar"></span>
-                        </button>
-                    </div>
-                    {links}
-                </div>
-            </nav>
-        )
+  isLoggedIn: function () {
+    return Parse.User.current() != null;
+  },
+  componentDidMount(){
+    jQuery('.collapse').collapse();
+  },
+  render: function () {
+    var links = (<AnonymousLinks/>);
+    if (this.isLoggedIn()) {
+      links = (<LoggedInLinks user={Parse.User.current()}/>);
     }
+
+    return (
+      <nav className="navbar navbar-default navbar-fixed-top">
+        <div className="container-fluid">
+          <div className="navbar-header">
+            <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
+                    data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+              <span className="sr-only">Toggle navigation</span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+              <span className="icon-bar"></span>
+            </button>
+          </div>
+          {links}
+        </div>
+      </nav>
+    )
+  }
 })
 
 export default TopNav
