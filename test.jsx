6e789b5339ee59fc339b3b4ad1ca01dbb5a2a85f
Neil Poulin <neil.j.poulin@gmail.com>
2017-06-10 12:31:41 -0600

    test

---
+++ test.jsx
@@ -0,0 +1,6 @@
+import React from "react"
+import {render} from "react-dom"
+
+const div = <div>Test Div</div>
+
+render({div}, document.getElementById("app"))
---
+++ webpack.test.js
@@ -0,0 +1,94 @@
+const webpack = require("webpack")
+require("babel-polyfill")
+const path = require("path")
+// const ExtractTextPlugin = require("extract-text-webpack-plugin");
+// const OptimizeCssAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+// const extractCss = new ExtractTextPlugin({
+//     filename: "styles.css", allChunks: false
+// });
+
+process.traceDeprecation = true
+
+module.exports = {
+    entry: {
+        // style: [path.join(__dirname, "src", "scss", "index.scss")],
+        scripts: [
+            "babel-polyfill",
+            path.join(__dirname, "test.jsx"),
+            // path.join(__dirname, "src", "scss", "index.scss")
+        ]
+    },
+    output: {
+        path: path.join(__dirname, "/public/test"),
+        filename: "[name].js",
+        // publicPath: "http://dev.oneroost.com/static/bundle",
+    },
+
+    // devtool: "source-map",
+    module: {
+        rules: [
+            // {
+            //     test: /\.json$/,
+            //     use: "json-loader"
+            // },
+            {
+                test: /\.js[x]?$/,
+                exclude: /(node_modules|bower_components|cloud|build)/,
+                // include: [path.join(__dirname, "src")],
+                loader: "babel-loader",
+                options: {
+                    presets: [
+                        "react",
+                        "es2015"
+                        // "stage-3"
+                    ],
+                    // plugins: ["syntax-async-functions", "transform-async-to-generator", "jsx-display-if"]
+                }
+            },
+            // {
+            //     test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+            //     loader: "file-loader?name=fonts/[name].[ext]"
+            // },
+            // {
+            //     test: /\.scss$/,
+            //     loader: ExtractTextPlugin.extract({
+            //         fallback: "style-loader",
+            //         use: [
+            //             {
+            //                 loader: "css-loader",
+            //                 options: {sourceMap: true}
+            //             },
+            //             {
+            //                 loader: "sass-loader",
+            //                 options: {sourceMap: true, }
+            //             },
+            //             {
+            //                 loader: "sass-resources-loader",
+            //                 options: {
+            //                     resources: "./src/scss/sass-resources.scss",
+            //                     sourceMap: true,
+            //                 }
+            //             }
+            //         ]
+            //     })
+            // }
+        ],
+    },
+    plugins: [
+        // extractCss,
+        // new webpack.ProvidePlugin({
+        //     $: "jquery",
+        //     jQuery: "jquery",
+        //     Tether: "tether",
+        // }),
+        // new webpack.NoEmitOnErrorsPlugin(),
+        // new webpack.DefinePlugin({"process.env": {NODE_ENV: JSON.stringify("production")}}),
+        // new webpack.optimize.UglifyJsPlugin({sourceMap: true}),
+        // new OptimizeCssAssetsPlugin()
+    ],
+    resolve: {
+        extensions: [".js", ".jsx"],
+        modules: ["node_modules"]
+    },
+    context: __dirname
+};
