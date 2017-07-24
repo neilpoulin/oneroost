20f9108cc0c887a5c597fc41bd49fd88e3cbde65
Neil Poulin <neil.j.poulin@gmail.com>
2017-07-24 08:23:07 -0600

    updates to dashboard loading

--- src/jsx/ducks/dashboard.js
+++ src/jsx/ducks/dashboard.js
@@ -1,5 +1,10 @@
-import {Map} from "immutable"
+import {fromJS, Set} from "immutable"
 import {LOGOUT} from "ducks/user"
+import * as User from "models/User"
+import * as Account from "models/Account"
+import * as Stakeholder from "models/Stakeholder"
+import Parse from "parse"
+import {normalize} from "normalizr"
 
 export const SHOW_ARCHIVED = "oneroost/dashboard/SHOW_ARCHIVED"
 export const HIDE_ARCHIVED = "oneroost/dashboard/HIDE_ARCHIVED"
@@ -8,17 +13,35 @@ export const RESET = "oneroost/dashboard/RESET"
 export const SET_TEMPALTE_ID = "oneroost/dashboard/SET_TEMPALTE_ID"
 export const SET_EXPORT_CSV_DATA = "oneroost/dashboard/SET_EXPORT_CSV_DATA"
 
-const initialState = Map({
+export const LOAD_DASHBOARD_REQUEST = "oneroost/dashboard/LOAD_DASHBOARD_REQUEST"
+export const LOAD_DASHBOARD_SUCCESS = "oneroost/dashboard/LOAD_DASHBOARD_SUCCESS"
+export const LOAD_DASHBOARD_ERROR = "oneroost/dashboard/LOAD_DASHBOARD_ERROR"
+
+const initialState = fromJS({
     isLoading: false,
     sortBy: null,
     searchTerm: "",
     sortDirection: null,
     showArchived: false,
     selectedTemplateId: null,
+    roosts: {},
+    templateIds: [],
     csvData: null
 });
 export default function reducer(state=initialState, action) {
     switch (action.type) {
+        case LOAD_DASHBOARD_REQUEST:
+            state = state.set("isLoading", true);
+            break;
+        case LOAD_DASHBOARD_SUCCESS:
+            state = state.set("isLoading", false)
+            state = state.set("stakeholders", action.payload.get("stakeholders"))
+            state = state.set("roosts", action.payload.get("roosts"))
+            state = state.set("templateIds", action.payload.get("templateIds"))
+            break;
+        case LOAD_DASHBOARD_ERROR:
+            state = state.set("isLoading", false)
+            break;
         case SHOW_ARCHIVED:
             state = state.set("showArchived", true)
             break;
@@ -45,6 +68,20 @@ export default function reducer(state=initialState, action) {
             break;
     }
     return state;
+}
+
+// Queries
+function getRoostsForAccount(accountId){
+    let accountUserQuery = new Parse.Query(User.className)
+    accountUserQuery.equalTo("account", Account.Pointer(accountId))
+
+    let query = new Parse.Query("Stakeholder")
+    query.matchesQuery("user", accountUserQuery)
+        .include("deal.template")
+        .include("user")
+        .include("template")
+        .include("invitedBy.account")
+    return query.find()
 }
 
 export const searchOpportunities = (query) => (dispatch, getState) => {
@@ -80,5 +117,91 @@ export const setExportCsvData = (data) => {
     return {
         type: SET_EXPORT_CSV_DATA,
         payload: data
+    }
+}
+
+export function loadDashboard(){
+    return (dispatch, getState) => {
+        dispatch({type: LOAD_DASHBOARD_REQUEST})
+        const state = getState()
+        let accountId = state.user.get("accountId");
+        let currentUserId = state.user.get("userId");
+        if (!accountId){
+            throw new Error("No account ID Oh my!!!")
+        }
+        getRoostsForAccount(accountId).then(stakeholders => {
+            stakeholders = stakeholders.map(p => p.toJSON())
+            let templateIds = Set([])
+            let roosts = stakeholders.reduce((map, stakeholder) => {
+                let dealId = stakeholder.deal.objectId;
+                templateIds = templateIds.add(stakeholder.deal.template.objectId)
+                if (!map.hasOwnProperty(dealId)){
+                    let {
+                        dealName,
+                        budget: {low: budgetLow, budgetHigh},
+                        lastActiveAt,
+                        readyRoostSubmitted,
+                        department,
+                        departmentCategory,
+                        departmentSubCategory,
+                    } = stakeholder.deal
+
+                    let roost = {
+                        dealId,
+                        templateId: stakeholder.deal.templateId,
+                        templateOwner: stakeholder.deal.template ? stakeholder.deal.template.ownedBy : null,
+                        dealName,
+                        budgetLow,
+                        budgetHigh,
+                        lastActiveAt,
+                        readyRoostSubmitted,
+                        department,
+                        departmentCategory,
+                        departmentSubCategory,
+                        approver: stakeholder.readyRoostApprover,
+                        stakeholders: [],
+                        hasAccess: false,
+                        status: "NOT SET",
+                    }
+
+                    map[dealId] = roost
+                }
+
+                let roost = map[dealId]
+                // set up user specific stuff
+                roost.stakeholders.push(stakeholder.user)
+                if (stakeholder.user.objectId === currentUserId){
+                    let isApprover = stakeholder.readyRoostApprover.objectId === currentUserId
+                    roost = {...roost,
+                        archived: !stakeholder.active,
+                        inviteAccepted: stakeholder.inviteAccepted,
+                        isApprover,
+                        invitedByUserId: stakeholder.invitedBy ? stakeholder.invitedBy.objectId : null,
+                        invitedByUserEmail: stakeholder.invitedBy ? stakeholder.invitedBy.username : null,
+                    }
+                }
+                map[dealId] = roost
+                return map
+            }, {})
+
+            dispatch({
+                type: LOAD_DASHBOARD_SUCCESS,
+                payload: {
+                    stakeholders,
+                    roosts,
+                    templateIds,
+                },
+                entities: normalize(stakeholders, [Stakeholder.Schema]).entities
+            })
+        }).catch(error => {
+            dispatch({
+                type: LOAD_DASHBOARD_ERROR,
+                error: {
+                    level: "error",
+                    message: "Something went wrong loading the dashboard",
+                    error,
+                }
+            })
+        })
     }
 }
